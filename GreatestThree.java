f1c44b08db41e453a395ebc52e1cab3c18241592
Rahulsavarala <152969762+Rahulsavarala@users.noreply.github.com>
2023-12-26 14:34:31 +0530

    Add files via upload

--- GreatestThree.java
+++ GreatestThree.java
@@ -1,42 +1,50 @@
+// Program to take three numbers from the user and print the greatest number.
+
 import java.util.Scanner;
 
 public class GreatestThree
 {
-   public static void main(String args[])
-   {
-     Scanner reader new Scanner(System.in);
-     System.out.print("\nEnter the numbers-------");
-     System.out.print("\nEnter the first number=");
-     Int fn=reader nextInt();
-     System.out.print("\nEnter the second number"); 
-     int sn reader.nextInt();
-     System.out.print("\nEnter the third number");
-     int tn reader.nextInt();
-
-     reader.close();
-
-if(fn>sn)
-  {
-    if(fn>tn)
-      {
-         system.out.print("\nThe greatest no="+fn);
-      }
-      else
-      {
-         system.out.print("\nThe greatest no="+tn);
-      }
+	public static void main (String args[])
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter Three numbers ------");
+		System.out.println("1st Number =  ");
+		int n1 = reader.nextInt();
+		System.out.println("2nd Number =  ");
+		int n2 = reader.nextInt();
+		System.out.println("3rd Number =  ");
+		int n3 = reader.nextInt();
+		reader.close();
+		if (n1 > n2)
+		{
+			if (n1 > n3)
+				{
+					System.out.println("Greatest among " + n1 + ", "  + n2 + " and " + n3 + " is " + n1);
+				}
+			else
+				{
+					System.out.println("Greatest among " + n1 + ", " + n2 + " and " + n3  + " is " + n3);
+				}
+		}
+		else
+		{
+			if (n2 > n3)
+				{
+					System.out.println("Greatest among " + n1 + ", " + n2 + " and " + n3 + " is " + n2);
+				}
+			else
+				{
+					System.out.println("Greatest among " + n1 + ", " + n2 + " and " + n3 + " is " + n3);
+				}
+		}
+	}
 }
-else
-{
-  if(sn>tn)
-    {
-      system.out.print("\nThe greatest no="+sn);
-    }
-    else
-    {
-      system.out.print("\nThe greatest no="+tn);
-    }
-    }
-}
-//java
-        
+//Output:
+//Enter Three numbers ------
+//1st Number =
+//5
+//2nd Number =
+//6
+//3rd Number =
+//3
+//Greatest among 5, 6 and 3 is 6
--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -15,3 +15,17 @@ public class MultiplicationTable
 		}
 	}
 }
+
+
+//Output:
+//Enter the number for which table is required. Number = 5
+//5 x 1  = 5
+//5 x 2  = 10
+//5 x 3  = 15
+//5 x 4  = 20
+//5 x 5  = 25
+//5 x 6  = 30
+//5 x 7  = 35
+//5 x 8  = 40
+//5 x 9  = 45
+//5 x 10  = 50
